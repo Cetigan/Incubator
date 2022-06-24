fd0d844e51f916353d6af6e73c302bd0195fa28e
cetigan <volosin.anatoli@gmail.com>
2022-06-24 19:02:20 +0300

    7-commit

---
+++ codewars/Powers of 2.js
@@ -0,0 +1,10 @@
+function powersOfTwo(n) {
+let a = [1]
+for (let i = 1;i<=n;i++){
+   a.push(Math.pow(2, i))}
+    return a}
+   
+   
+   
+
+powersOfTwo(0)
---
+++ codewars/u Find Maximum and Minimum Values of a List.js
@@ -0,0 +1,16 @@
+var min = function (list) {
+   for (let i = 0; i < list.length; i++) {
+      let a = Math.min(... list)
+      return  a 
+     
+
+}}
+
+var max = function (list) {
+   for (let i =0; i< list.length;i++){
+      let a = Math.max(...list)
+      return a}
+
+   }
+    
+
